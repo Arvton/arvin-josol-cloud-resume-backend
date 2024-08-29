ea3aa9185bed2c8f57e3e2c2e8099e8e54e8be7b
Arvin Josol <arvin.josol@live.com>
2024-08-29 15:22:29 -0700

    Add new GET function: - use the same logic from the POST function     - use GET method instead of POST     - do not increment the Count value

---
+++ src/functions/GetHTTPTrigger.js
@@ -0,0 +1,36 @@
+const { app } = require('@azure/functions');
+const { TableClient, AzureNamedKeyCredential } = require('@azure/data-tables');
+
+const tableName = 'Visitors';
+const partitionKey = '1';
+const rowKey = '1';
+
+const account = process.env['COSMOS_ACCOUNT']; // Get the account name from environment variables
+const accountKey = process.env['COSMOS_ACCOUNT_KEY']; // Get the account key from environment variables
+
+const sharedKeyCredential = new AzureNamedKeyCredential(account, accountKey);
+const tableClient = new TableClient(`https://${account}.table.cosmos.azure.com:443/`, tableName, sharedKeyCredential);
+
+app.http('GetHTTPTrigger', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        // Retrieve the existing entity
+        const entity = await tableClient.getEntity(partitionKey, rowKey);
+
+        // Remove the 'odata.metadata' property
+        delete entity['odata.metadata'];
+
+        // Set the HTTP response
+        context.res = {
+            status: 200,
+            body: JSON.stringify({ Count: entity.Count }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        // Return the HTTP response
+        return context.res;
+    },
+});
